3ce5208a47a80f42fc466b6f8ed975c165a4b312
Robin Shin <shin.robin@icloud.com>
2023-08-23 13:12:57 +0200

    chore: rework gswitch alias

--- .config/aliases/common.sh
+++ .config/aliases/common.sh
@@ -6,6 +6,9 @@ alias tf="terraform"
 
 alias notebook_venv="source $HOME/.venv_jupyter/bin/activate && jupyter notebook --port=8889"
 
-alias gswitch='gcloud config configurations activate'
 alias gcswitch='gcloud container clusters get-credentials $CLUSTER_NAME --zone $GCP_ZONE --project $GCP_PROJECT_ID'
 
+gswitch() {
+  gcloud config configurations activate $1
+  export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/gcloud_env/application_default_credentials_$1.json
+}
